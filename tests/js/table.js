4abc9424ce70b00d921ebfa14016a839ddafd09e
ashleyisles <aisles@smu.edu>
2016-07-13 18:42:44 +0200

    Added cool table!

--- tests/js/table.js
+++ tests/js/table.js
@@ -1,16 +1,20 @@
 $(document).ready(function() {
 
     var repo = "http://higeomes.i3mainz.hs-mainz.de/openrdf-sesame/repositories/textelsem";
+
     var query = "PREFIX higeomes: <http://higeomes.i3mainz.hs-mainz.de/textelsem/ArchDB/>"
-                +"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
-                +"SELECT DISTINCT ?t1 ?f1 ?kind ?pop\n"
-                +"WHERE {"
-                +    "?t1 higeomes:hasFindspot ?f1 ."
-  	            +    "?f1 higeomes:state ?statement ."
-  	            +    "?statement higeomes:kind ?kind ."
-  	            +    "?related higeomes:definedTopo ?t1 ."
-  	            +    "?related higeomes:definedPopulation ?pop ."
-                +"}";
+            +"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
+            +"SELECT ?t1 ?f1 ?kind_label ?pop_label ?category\n"
+            +"WHERE {"
+            +"    ?t1 higeomes:hasFindspot ?f1 ."
+            +"    ?f1 higeomes:state ?statement ."
+            +"    ?statement higeomes:kind ?kind ."
+            +"    ?kind rdfs:label ?kind_label ."
+            +"    ?related higeomes:definedTopo ?t1 ."
+            +"    ?related higeomes:definedPopulation ?pop ."
+            +"    ?pop higeomes:hasCategory ?category ."
+            +"    ?pop rdfs:label ?pop_label ."
+            +"}"
 
     $.ajax({
         url: repo,
@@ -22,27 +26,38 @@ $(document).ready(function() {
         },
         success: function(data) {
             var row = data.results.bindings;
-            var values = [];
-            var compare = row[0].pop.value;
-            var regex = /-\d+/;
+            console.log(row);
 
+            var population = [];
+            var kind = [];
             for(var i in row)
             {
-                if(row[i].pop.value == compare) {
-                    continue;
-                }
-                else {
-                    values.push(compare);
-                    compare = row[i].pop.value
+                population.push(row[i].pop_label.value);
+                kind.push(row[i].kind_label.value);
+            }
+
+            $.unique(population);
+            $.unique(kind);
+
+            for(var i in kind)
+            {
+                $('#header_details>tr').append("<th class='two wide'>"+ kind[i] +"</th>")
+            }
+
+            for(var i in population)
+            {
+
+                $('#table_details').append("<tr id='"+ population[i].replace(/\s+|\W+/g, '') +"'><td>"+ population[i] +"</td></tr>");
+                for(var j in kind)
+                {
+                    $('#' + population[i].replace(/\s+|\W+/g, '')).append("<td id='"+ kind[j].replace(/\s+|\W+/g, '') +"'></td>")
                 }
             }
 
-            values = values.sort(function(a, b) {
-                console.log(a);
-                return parseFloat(regex.exec(a).replace('-', '')) < parseFloat(regex.exec(b).replace('-', ''));
-            });
-
-            console.log(values);
+            for(var i in row)
+            {
+                $("#" + row[i].pop_label.value.replace(/\s+|\W+/g, '') + ">#" + row[i].kind_label.value.replace(/\s+|\W+/g, '')).append("<i class='mini circle icon'></i>");
+            }
         }
     })
 });
