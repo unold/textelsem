de7eaa66141d9c11de6b4c6674d7a2dd3124ceab
ashleyisles <aisles@smu.edu>
2016-07-18 14:02:37 +0200

    Added popup
    Might remove

--- tests/js/table.js
+++ tests/js/table.js
@@ -3,20 +3,21 @@ $(document).ready(function() {
     var repo = "http://higeomes.i3mainz.hs-mainz.de/openrdf-sesame/repositories/textelsem";
     var toponym_regex = /toponym\W\d+/;
 
-    var query = "PREFIX higeomes: <http://higeomes.i3mainz.hs-mainz.de/textelsem/ArchDB/>"
-            +"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
+    var query = "PREFIX higeomes: <http://higeomes.i3mainz.hs-mainz.de/textelsem/ArchDB/>\n"
+            +"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"
             +"SELECT ?t1 ?f1 ?kind_label ?pop_label ?category\n"
-            +"WHERE {"
-            +"    ?t1 higeomes:hasFindspot ?f1 ."
-            +"    ?f1 higeomes:state ?statement ."
-            +"    ?statement higeomes:kind ?kind ."
-            +"    ?kind rdfs:label ?kind_label ."
-            +"    ?related higeomes:definedTopo ?t1 ."
-            +"    ?related higeomes:definedPopulation ?pop ."
-            +"    ?pop higeomes:hasCategory ?category ."
-            +"    ?pop rdfs:label ?pop_label ."
+            +"WHERE {\n"
+            +"    ?t1 higeomes:hasFindspot ?f1 .\n"
+            +"    ?f1 higeomes:state ?statement .\n"
+            +"    ?statement higeomes:kind ?kind .\n"
+            +"    ?kind rdfs:label ?kind_label .\n"
+            +"    ?related higeomes:definedTopo ?t1 .\n"
+            +"    ?related higeomes:definedPopulation ?pop .\n"
+            +"    ?pop higeomes:hasCategory ?category .\n"
+            +"    ?pop rdfs:label ?pop_label .\n"
             +"}"
 
+    console.log(query);
     $.ajax({
         url: repo,
         dataType: 'jsonp',
@@ -59,7 +60,7 @@ $(document).ready(function() {
 
             for(var i in kind)
             {
-                $('#header_details>tr').append("<th class='two wide'>"+ kind[i] +"</th>");
+                $('#header_details>tr').append("<th>"+ kind[i] +"</th>");
                 total[kind[i]] = 0;
             }
 
@@ -75,7 +76,7 @@ $(document).ready(function() {
 
             for(var i in row)
             {
-                $("#" + row[i].pop_label.value.replace(/\s+|\W+/g, '') + ">#" + row[i].kind_label.value.replace(/\s+|\W+/g, '')).append("<i class='mini circle icon'></i>");
+                $("#" + row[i].pop_label.value.replace(/\s+|\W+/g, '') + ">#" + row[i].kind_label.value.replace(/\s+|\W+/g, '')).append("<i class='mini circle icon' data-content='"+ row[i].t1.value +"' data-variation='mini'></i>");
 
                 for(var x in categories)
                 {
@@ -93,16 +94,16 @@ $(document).ready(function() {
                 total[row[i].kind_label.value]++;
             }
 
-            console.log(significance(row, grid, total));
+            $('.mini.circle.icon').popup();
 
             $("#table_details>tr").click(function() {
-                console.log($(this).text());
+
                 $(".content>.header").html($(this).text().toString()[0].toUpperCase() + $(this).text().toString().slice(1));
                 $(".content>.meta").html(grid[$(this).text()]["category"] + "<div class='ui divider'></div>");
                 $(".content>.description").html("<div class='ui text'>Click to show related toponyms.</div><br><div class='ui fluid styled accordion'></div>");
                 for(var i in kind)
                 {
-                    $('.ui.fluid.styled.accordion').append("<div class='title'><i class='dropdown icon'></i>" + kind[i] + "</div><div class='content'><div class='ui relaxed divided link list' id='" + kind[i].replace(/\s+|\W+/g, '') + "_list'></div></div>");
+                    $('.ui.fluid.styled.accordion').append("<div class='title'><i class='dropdown icon'></i>" + kind[i] + "</div><div class='content'><div class='ui relaxed divided selection list' id='" + kind[i].replace(/\s+|\W+/g, '') + "_list'></div></div>");
                 }
 
                 for(var i in grid[$(this).text()].slice(0, grid[$(this).text()].length))
@@ -114,10 +115,9 @@ $(document).ready(function() {
                     }
                     for(var j in grid[$(this).text()][i]["related_toponyms"])
                     {
-                        $("#" + grid[$(this).text()][i]["kind_name"].replace(/\s+|\W+/g, '') + "_list").append("<a class='item' href='" + grid[$(this).text()][i]["related_toponyms"][j] + "'>"+toponym_regex.exec(grid[$(this).text()][i]["related_toponyms"][j])+"</a>");
+                        $("#" + grid[$(this).text()][i]["kind_name"].replace(/\s+|\W+/g, '') + "_list").append("<div class='item'><i class='marker icon'></i><div class='content'>"+toponym_regex.exec(grid[$(this).text()][i]["related_toponyms"][j])+"</div></div>");
                     }
                 }
-                // $(".extra.content").html("<div class='ui divider'></div><div class='right floated'>Whatsup</div>");
                 $(".ui.modal").modal('show');
                 $('.ui.accordion').accordion();
             });
